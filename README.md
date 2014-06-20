34ce27517d49d55ea4b40aaae65d90d4b7834d28
robin4002 <robin4002@gmail.com>
2014-06-21 01:03:26 +0200

    Fisrt commit

---
+++ README.md
@@ -0,0 +1,5 @@
+SysInfoMod
+============
+
+You're not sure of performance promised by your host ? <br>
+Through SysInfoMod, you will be able to check in two seconds !
---
+++ src/fr/mcnanotech/SysInfo.java
@@ -0,0 +1,140 @@
+package fr.mcnanotech;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import net.minecraft.util.Util;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "sysinfo", name = "System Information", version = "1.0.0")
+public class SysInfo
+{
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		ArrayList<String> info = new ArrayList<String>();
+
+		info.add("--- System.getProperty ---");
+		info.add("Java vendor : " + System.getProperty("java.vendor"));
+		info.add("Java version : " + System.getProperty("java.version"));
+		info.add("OS arch : " + System.getProperty("os.arch"));
+		info.add("OS name : " + System.getProperty("os.name"));
+		info.add("OS version : " + System.getProperty("os.version"));
+
+		info.add("--- Runtime information ---");
+		info.add("Number of core : " + Runtime.getRuntime().availableProcessors());
+		info.add("Free memory (mo): " + Runtime.getRuntime().freeMemory() / (1024 * 1024));
+		info.add("Maximum memory (mo): " + (Runtime.getRuntime().maxMemory() == Long.MAX_VALUE ? "no limit" : Runtime.getRuntime().maxMemory() / (1024 * 1024)));
+		info.add("Total memory (mo): " + Runtime.getRuntime().totalMemory() / (1024 * 1024));
+
+		info.add("--- JVM arg ---");
+		info.add(getJVMFlag());
+		
+		info.add("--- Disk information ---");
+		File[] roots = File.listRoots();
+		for(File root : roots)
+		{
+			info.add("File system root: " + root.getAbsolutePath());
+			info.add("Total space (mo): " + root.getTotalSpace() / (1024 * 1024));
+			info.add("Free space (mo): " + root.getFreeSpace() / (1024 * 1024));
+			info.add("Usable space (mo): " + root.getUsableSpace() / (1024 * 1024));
+		}
+
+		info.add("--- System information ---");
+		if(Util.getOSType() == Util.EnumOS.WINDOWS)
+		{
+			executeCommand(info, "SYSTEMINFO");
+		}
+		else if(Util.getOSType() == Util.EnumOS.LINUX)
+		{
+			executeCommand(info, "ps -eH");
+			executeCommand(info, "free");
+		}
+		info.add("--- SysInfo finish ---");
+		
+		File sysInfoDir = new File(".", "SysInfo");
+		sysInfoDir.mkdirs();
+		System.out.println(sysInfoDir.getAbsolutePath());
+		Date date = new Date();
+		DateFormat shortDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+		String fileName = shortDateFormat.format(date).replace('/', '.').replace(' ', '-').replace(':', '.') + ".txt";
+		File target = new File(sysInfoDir, fileName);
+		System.out.println(target.getAbsolutePath());
+		try
+		{
+			target.createNewFile();
+			BufferedWriter bw = new BufferedWriter(new FileWriter(target));
+			for(String str : info)
+			{
+				bw.write(str);
+				bw.newLine();
+			}
+			bw.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private ArrayList<String> executeCommand(ArrayList<String> list, String command)
+	{
+		try
+		{
+			String line;
+			Process p = Runtime.getRuntime().exec(command);
+			list.add("-- Running command : " + command + " --");
+			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			while((line = input.readLine()) != null)
+			{
+				list.add(line);
+			}
+			input.close();
+			list.add("-- End of the command --");
+		}
+		catch(Exception err)
+		{
+			System.out.println(err.getMessage());
+		}
+		return list;
+	}
+	
+	private String getJVMFlag()
+	{
+        RuntimeMXBean runtimemxbean = ManagementFactory.getRuntimeMXBean();
+        List list = runtimemxbean.getInputArguments();
+        int i = 0;
+        StringBuilder stringbuilder = new StringBuilder();
+        Iterator iterator = list.iterator();
+
+        while (iterator.hasNext())
+        {
+            String s = (String)iterator.next();
+
+            if (s.startsWith("-X"))
+            {
+                if (i++ > 0)
+                {
+                    stringbuilder.append(" ");
+                }
+
+                stringbuilder.append(s);
+            }
+        }
+
+        return String.format("%d total; %s", new Object[] {Integer.valueOf(i), stringbuilder.toString()});
+	}
+}
