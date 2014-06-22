cc1211acc70ecf6d04e1f08f4cf3e9294aedfec8
kevin68 <baumeyerkevin@gmx.fr>
2014-06-22 13:34:01 +0200

    Auto upload files

--- src/fr/mcnanotech/sysinfo/SysInfo.java
+++ src/fr/mcnanotech/sysinfo/SysInfo.java
@@ -3,20 +3,25 @@ package fr.mcnanotech.sysinfo;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.net.SocketException;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.net.ftp.FTPClient;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
 
 @Mod(modid = "sysinfo", name = "System Information", version = "1.0.3")
 public class SysInfo
@@ -41,7 +46,7 @@ public class SysInfo
 
 		info.add("--- JVM arg ---");
 		info.add(getJVMFlag());
-		
+
 		info.add("--- Disk information ---");
 		File[] roots = File.listRoots();
 		for(File root : roots)
@@ -65,7 +70,7 @@ public class SysInfo
 			executeCommand(info, "cat /proc/cpuinfo");
 		}
 		info.add("--- SysInfo finish ---");
-		
+
 		File sysInfoDir = new File(".", "SysInfo");
 		sysInfoDir.mkdirs();
 		Date date = new Date();
@@ -82,6 +87,26 @@ public class SysInfo
 				bw.newLine();
 			}
 			bw.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+
+		try
+		{
+			if(event.getSide() == Side.CLIENT)
+			{
+				upload(target, "clients/");
+			}
+			else
+			{
+				upload(target, "servers/");
+			}
+		}
+		catch(SocketException e)
+		{
+			e.printStackTrace();
 		}
 		catch(IOException e)
 		{
@@ -110,45 +135,59 @@ public class SysInfo
 		}
 		return list;
 	}
-	
+
 	private String getJVMFlag()
 	{
-        RuntimeMXBean runtimemxbean = ManagementFactory.getRuntimeMXBean();
-        List list = runtimemxbean.getInputArguments();
-        int i = 0;
-        StringBuilder stringbuilder = new StringBuilder();
-        Iterator iterator = list.iterator();
+		RuntimeMXBean runtimemxbean = ManagementFactory.getRuntimeMXBean();
+		List list = runtimemxbean.getInputArguments();
+		int i = 0;
+		StringBuilder stringbuilder = new StringBuilder();
+		Iterator iterator = list.iterator();
 
-        while (iterator.hasNext())
-        {
-            String s = (String)iterator.next();
+		while(iterator.hasNext())
+		{
+			String s = (String)iterator.next();
 
-            if (s.startsWith("-X"))
-            {
-                if (i++ > 0)
-                {
-                    stringbuilder.append(" ");
-                }
+			if(s.startsWith("-X"))
+			{
+				if(i++ > 0)
+				{
+					stringbuilder.append(" ");
+				}
 
-                stringbuilder.append(s);
-            }
-        }
+				stringbuilder.append(s);
+			}
+		}
 
-        return String.format("%d total; %s", new Object[] {Integer.valueOf(i), stringbuilder.toString()});
+		return String.format("%d total; %s", new Object[] {Integer.valueOf(i), stringbuilder.toString()});
 	}
-	
-    private EnumOS getOSType()
-    {
-        String s = System.getProperty("os.name").toLowerCase();
-        return s.contains("win") ? EnumOS.WINDOWS : (s.contains("mac") ? EnumOS.MACOS : (s.contains("solaris") ? EnumOS.SOLARIS : (s.contains("sunos") ? EnumOS.SOLARIS : (s.contains("linux") ? EnumOS.LINUX : (s.contains("unix") ? EnumOS.LINUX : EnumOS.UNKNOWN)))));
-    }
-    
-    private enum EnumOS
-    {
-        LINUX,
-        SOLARIS,
-        WINDOWS,
-        MACOS,
-        UNKNOWN;
-    }
+
+	private EnumOS getOSType()
+	{
+		String s = System.getProperty("os.name").toLowerCase();
+		return s.contains("win") ? EnumOS.WINDOWS : (s.contains("mac") ? EnumOS.MACOS : (s.contains("solaris") ? EnumOS.SOLARIS : (s.contains("sunos") ? EnumOS.SOLARIS : (s.contains("linux") ? EnumOS.LINUX : (s.contains("unix") ? EnumOS.LINUX : EnumOS.UNKNOWN)))));
+	}
+
+	private enum EnumOS
+	{
+		LINUX, SOLARIS, WINDOWS, MACOS, UNKNOWN;
+	}
+
+	public static void upload(File file, String folder) throws SocketException, IOException
+	{
+		FTPClient client_ftp = new FTPClient();
+
+		FileInputStream f_stream = null;
+
+		client_ftp.connect("mcnanotech.fr");
+		client_ftp.login("@@@@@@@@", "********");
+
+		String fichier = file.getAbsolutePath();
+		f_stream = new FileInputStream(fichier);
+
+		client_ftp.storeFile("/" + folder + file.getName(), f_stream);
+
+		client_ftp.logout();
+		f_stream.close();
+	}
 }
